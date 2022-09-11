9da45e38f8c4fb53b72bbc7cbacf9546b522f75a
Cesar Daniel Meneses <cdanielmg200@gmail.com>
2022-09-11 16:34:52 -0500

    Refactor WordCounterDefauld class

--- src/main/java/com/cedaniel200/semillero/counter/WordCounterDefault.java
+++ src/main/java/com/cedaniel200/semillero/counter/WordCounterDefault.java
@@ -6,16 +6,28 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 public class WordCounterDefault implements WordCounter{
     @Override
     public Summary process(String pathFile) throws IOException {
         Path path = Paths.get(pathFile);
-        long numberOfLines = Files.lines(path).count();
-        long numberOfWords = Files.lines(path)
-                .map(line -> line.split(" "))
-                .mapToInt(words -> words.length)
-                .sum();
+        long numberOfLines = getNumberOfLines(path);
+        long numberOfWords = getNumberOfWord(path);
         return new Summary(numberOfLines, numberOfWords);
+    }
+
+    private long getNumberOfLines(Path path) throws IOException {
+        try(Stream<String> lines = Files.lines(path)){
+            return lines.count();
+        }
+    }
+
+    private long getNumberOfWord(Path path) throws IOException {
+        try(Stream<String> lines = Files.lines(path)){
+            return lines.map(line -> line.split(" "))
+                    .mapToInt(words -> words.length)
+                    .sum();
+        }
     }
 }
