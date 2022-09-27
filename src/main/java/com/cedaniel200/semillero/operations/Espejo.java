9959e140b20e7c9c1385a7cee759fd46fe0a6220
Cesar Daniel Meneses <cdanielmg200@gmail.com>
2022-09-27 11:19:37 -0500

    refactor y configuracion

--- src/main/java/com/cedaniel200/semillero/operations/Espejo.java
+++ src/main/java/com/cedaniel200/semillero/operations/Espejo.java
@@ -4,14 +4,17 @@ import java.util.Arrays;
 
 public class Espejo {
     public long sum(int... numbers){
-        return Arrays.stream(numbers).sum();
+         Arrays.stream(numbers).sum();
+         return Arrays.stream(numbers).sum();
     }
 
     public long count(int... numbers){
+        Arrays.stream(numbers).count();
         return Arrays.stream(numbers).count();
     }
 
     public double average(int... numbers){
+        Arrays.stream(numbers).average().orElse(0.0);
         return Arrays.stream(numbers).average().orElse(0.0);
     }
 }
--- src/main/java/com/cedaniel200/semillero/operations/Operator.java
+++ src/main/java/com/cedaniel200/semillero/operations/Operator.java
@@ -5,14 +5,32 @@ import java.util.Arrays;
 public class Operator {
 
     public long sum(int... numbers){
+        Arrays.stream(numbers).sum();
         return Arrays.stream(numbers).sum();
     }
 
     public long count(int... numbers){
+        Arrays.stream(numbers).count();
         return Arrays.stream(numbers).count();
     }
 
     public double average(int... numbers){
+        Arrays.stream(numbers).average().orElse(0.0);
+        return Arrays.stream(numbers).average().orElse(0.0);
+    }
+
+    public long sum2(int... numbers){
+        Arrays.stream(numbers).sum();
+        return Arrays.stream(numbers).sum();
+    }
+
+    public long count2(int... numbers){
+        Arrays.stream(numbers).count();
+        return Arrays.stream(numbers).count();
+    }
+
+    public double average2(int... numbers){
+        Arrays.stream(numbers).average().orElse(0.0);
         return Arrays.stream(numbers).average().orElse(0.0);
     }
 }
