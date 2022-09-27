b38b3589927df2d98ef753c48487d20af61766e4
César Daniel Meneses Guevara <cdanielmg200@gmail.com>
2022-09-27 00:51:21 -0500

    Feature/counter (#7)
    Se agrega el Jenkinsfile

---
+++ Jenkinsfile
@@ -0,0 +1,36 @@
+pipeline {
+    agent {label "windows"}
+    stages {
+        stage('build') {
+            steps {
+                bat 'gradlew clean build -x test'
+            }
+        }
+        stage('test') {
+            steps {
+                bat "gradlew test"
+            }
+        }
+        stage('coverage') {
+            steps {
+                bat 'gradlew jacocoTestReport'
+            }
+        }
+        stage('Analisis de SonarQube') {
+            steps {
+                withSonarQubeEnv(installationName: 'SonarCloud', credentialsId: 'token-sonarcloud') {
+                    script {
+                        bat 'gradlew sonarqube -Dsonar.branch.targe=feature/counter -Dsonar.branch.name=feature/counter'
+                    }
+                }
+            }
+        }
+        stage("Quality Gate") {
+            steps {
+                timeout(time: 1, unit: 'HOURS') {
+                    waitForQualityGate abortPipeline: true
+                }
+            }
+        }
+    }
+}
--- src/test/java/com/cedaniel200/semillero/domain/counter/WordCounterTest.java
+++ src/test/java/com/cedaniel200/semillero/domain/counter/WordCounterTest.java
@@ -1,7 +1,8 @@
-package com.cedaniel200.semillero.counter;
+package com.cedaniel200.semillero.domain.counter;
 
 import com.cedaniel200.semillero.model.Summary;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
@@ -12,27 +13,31 @@ import java.nio.file.Paths;
 import static org.junit.jupiter.api.Assertions.*;
 
 class WordCounterTest {
+
+    private WordCounter wordCounter;
+    private String absolutePath;
+
+    @BeforeEach
+    public void setup(){
+        Path resourceDirectory = Paths.get("src","test","resources");
+        absolutePath = resourceDirectory.toFile().getAbsolutePath();
+        wordCounter = new WordCounterDefault();
+    }
+
     @Test
     void mustReturnCorrectSummary() throws IOException {
-        Path resourceDirectory = Paths.get("src","test","resources");
-        String absolutePath = resourceDirectory.toFile().getAbsolutePath();
-        WordCounter wordCounter = new WordCounterDefault();
-
         Summary summary = wordCounter.process(absolutePath + File.separator + "test.txt");
 
+        assertNotNull(summary);
         Assertions.assertEquals(2, summary.getNumberOfLines());
         Assertions.assertEquals(5, summary.getNumberOfWord());
     }
 
     @Test
-    void mustThromsIOExceptionWhenFileNotFound() throws IOException {
+    void mustThrowsIOExceptionWhenFileNotFound() {
         IOException thrown = assertThrows(
                 IOException.class,
                 () -> {
-                    Path resourceDirectory = Paths.get("src","test","resources");
-                    String absolutePath = resourceDirectory.toFile().getAbsolutePath();
-                    WordCounter wordCounter = new WordCounterDefault();
-
                     Summary summary = wordCounter.process(absolutePath + File.separator + "test23.txt");
                 },
                 "Expected doThing() to throw, but it didn't"
